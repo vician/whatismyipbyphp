efcb0aeca7615fc157c3a21f4ff54ef0ffdcfd04
Martin Vician <martin.vician@nic.cz>
2017-10-12 14:24:02 +0200

    fixup! added 6to4 prefix

--- 6to4.php
+++ 6to4.php
@@ -14,9 +14,12 @@ $parts = explode(".",$ip);
 
 if (count($parts) != 4) die($ip);
 
-$ipv6to4 = "2002";
-foreach ($parts as $part) {
-	$ipv6to4 .= ":".dechex($part);
+$ipv6to4 = "2002:";
+//foreach ($parts as $part) {
+for ($i = 0; $i < count($parts); $i++) {
+	// echo "$i: ".$parts[$i]." => ".dechex($parts[$i])." = ".($i % 2)."\n";
+	if ($i % 2 == 0) $ipv6to4 .= ":";
+	$ipv6to4 .= dechex($parts[$i]);
 }
 $ipv6to4 .= "::";
 
